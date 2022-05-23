ecf0406794d623251bb3f701b2486c4288b355ec
Anna Ivanova <iva.ann01@yandex.ru>
2022-05-23 16:49:48 +0500

    Initial Commit

---
+++ PassDataSeconPrj/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  PassDataSeconPrj
+//
+//  Created by Анна Иванова on 17.04.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var login: String?
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = self.login else { return }
+        label.text = "Hello, \(login)"
+    }
+    
+    @IBAction func goBack(_ sender: UIButton) {
+    }
+    
+    
+}
---
+++ PassDataSeconPrj/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  PassDataSeconPrj
+//
+//  Created by Анна Иванова on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    @IBAction func unwindSegueToMainSreen(segue: UIStoryboardSegue) {
+        guard let svc = segue.source as? SecondViewController else { return }
+        self.resultLabel.text = svc.label.text
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+
+}
+
